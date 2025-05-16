fabf2f4cc9f78af89293c62e97e6bc654e0fa0a2
Adrian Sokolowski <adriansokolowski7850@gmail.com>
2025-05-16 15:37:33 +0100

    Added new menu mp menu scene and set up network

---
+++ Microbial Mayhem/Assets/Scripts/Multiplayer/UILogic.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+using TMPro;
+
+public class UILogic : MonoBehaviour
+{
+    public Button hostButton;
+    public Button clientButton;
+    public TMPro.TMP_InputField ipInputField; // Optional
+
+    public GameObject hostObject;
+    public GameObject clientObject;
+    public GameObject inputFieldObject;
+
+    void Start()
+    {
+        hostButton.onClick.AddListener(StartHost);
+        clientButton.onClick.AddListener(StartClient);
+    }
+
+    void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+        hostObject.SetActive(false);
+        clientObject.SetActive(false);
+        inputFieldObject.SetActive(false);
+        Debug.Log("Host started.");
+    }
+
+    void StartClient()
+    {
+        string ip = ipInputField != null ? ipInputField.text : "127.0.0.1";
+
+        // Set the IP address if using UnityTransport
+        Unity.Netcode.Transports.UTP.UnityTransport transport =
+            (Unity.Netcode.Transports.UTP.UnityTransport)NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+        transport.SetConnectionData(ip, 7777); // Port can be customized
+
+        NetworkManager.Singleton.StartClient();
+        hostObject.SetActive(false);
+        clientObject.SetActive(false);
+        inputFieldObject.SetActive(false);
+        Debug.Log("Client connecting to: " + ip);
+    }
+}
